761b70793c283ef8debd79f1bf850ce6c3527d8f
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-13 12:50:46 +0100

    Added animation

--- src/components/W12MHeader/W12MHeader.tsx
+++ src/components/W12MHeader/W12MHeader.tsx
@@ -1,4 +1,17 @@
+import Lottie from 'react-lottie';
+import animationData from '../../lotties/alien.json';
+
+const defaultOptions = {
+	loop: true,
+	autoplay: true,
+	animationData: animationData,
+	rendererSettings: {
+	  preserveAspectRatio: "xMidYMid slice"
+	}
+  };
+
 const W12MHeader = () => (
+	<div>
 	<ul>
 		<li>Each species may only submit ONE W-12-M form.</li>
 		<li>
@@ -6,6 +19,12 @@ const W12MHeader = () => (
 			spared.
 		</li>
 	</ul>
+	<Lottie 
+			options={defaultOptions}
+			height={200}
+			width={200}
+	/>
+	</div>
 );
 
 export default W12MHeader;
